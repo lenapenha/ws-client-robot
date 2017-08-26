3dd1307839f9f4b45c387f6730164257186cceb5
Helena Penha <lenapenha02@hotmail.com>
2017-08-25 22:41:46 -0300

    ajuste de response

--- src/main/java/sample/websocket/SimpleClientWebSocketHandler.java
+++ src/main/java/sample/websocket/SimpleClientWebSocketHandler.java
@@ -3,13 +3,10 @@ package sample.websocket;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import sample.component.ReceivedTextAreaComponent;
 import sample.component.RegisterModel;
 
 import java.io.IOException;
@@ -17,6 +14,8 @@ import java.util.ArrayList;
 
 public class SimpleClientWebSocketHandler extends TextWebSocketHandler {
     private static final Gson gson = new GsonBuilder().create();
+    private static final String NAME_RASP = "Rasp";
+    private String namePeer;
 
     private WebSocketSession session;
     private boolean fwdBlocked;
@@ -29,7 +28,7 @@ public class SimpleClientWebSocketHandler extends TextWebSocketHandler {
 
 
         this.session = session;
-        RegisterModel registerModel = new RegisterModel("register", "Rasp");
+        RegisterModel registerModel = new RegisterModel("register", NAME_RASP);
 
         Gson gson = new Gson();
         getSession().sendMessage(new TextMessage(gson.toJson(registerModel)));
@@ -46,36 +45,6 @@ public class SimpleClientWebSocketHandler extends TextWebSocketHandler {
 //                try {
 //                    //comm.console.println("[HEX DATA]   " + event.getHexByteString());
 //                    comm.console.println("[ASCII DATA] " + event.getAsciiString());
-
-                    float sensorDistanceOne = 9999, sensorDistanceTwo = 9999, sensorDistanceTree = 9999,
-                            sensorDistanceFor = 9999, sensorDistanceFive = 9999, sensorDistanceSix = 9999;
-
-                    if(!fwdBlocked) {
-                        if(sensorDistanceOne <= 30 ||
-                                sensorDistanceTwo <= 15 || sensorDistanceTree <= 15 || sensorDistanceFor <= 15 || sensorDistanceFive <= 15) {
-                            fwdBlocked = true;
-                            sendComand("$pi.blk.fwd");
-                        }
-                    } else {
-                        if(sensorDistanceOne > 30 &&
-                                sensorDistanceTwo > 15 && sensorDistanceTree > 15 && sensorDistanceFor > 15 && sensorDistanceFive > 15) {
-                            fwdBlocked = false;
-                            sendComand("$pi.rls.fwd");
-                        }
-                    }
-
-                    if (!bwdBlocked) {
-                        if(sensorDistanceSix <= 15) {
-                            bwdBlocked = true;
-                            sendComand("$pi.blk.bwd");
-                        }
-                    } else {
-                        if(sensorDistanceSix > 15) {
-                            bwdBlocked = false;
-                            sendComand("$pi.rls. bwd");
-                        }
-                    }
-
 
 
 //                } catch (IOException e) {
@@ -100,18 +69,21 @@ public class SimpleClientWebSocketHandler extends TextWebSocketHandler {
         comands.add("$js.cmd.bwd");
         comands.add("$js.cmd.lft");
         comands.add("$js.cmd.rgt");
+        comands.add("$js.cmd.stp");
 
-        if (!message.equals("connectionResponseWS") && !message.equals("resgisterResponseWS")) {
+        if (!message.equals("connectionResponseWS") && !message.equals("registerResponseWS")) {
             String responseMsg = "received";
-
-            if(comands.contains(jsonMessage.get("cmd").getAsString())){
+            this.namePeer = jsonMessage.get("from").getAsString();
+            if(comands.contains(jsonMessage.get("message").getAsString())){
                 System.out.println("$pi.cmd.fwd");
 //                comm.serial.writeln("$pi.cmd.fwd");
             }
 
             JsonObject response = new JsonObject();
-            response.addProperty("id", "comandResponseRobot");
-            response.addProperty("response", responseMsg);
+            response.addProperty("id", "response");
+            response.addProperty("from", this.NAME_RASP);
+            response.addProperty("to", this.namePeer);
+            response.addProperty("message", responseMsg);
             session.sendMessage(new TextMessage(response.toString()));
         }
 
